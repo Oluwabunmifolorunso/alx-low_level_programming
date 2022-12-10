38e4fef5e9ec5be8f958c47cbb88d9f105e21ec8
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-09 18:49:51 -0800

    I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - How to make lowercase with putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sm;
+
+	for (sm = 'a'; sm <= 'z'; sm++)
+	{
+		putchar(sm);
+		putchar("\n");
+	}
+	return (0);
+}
