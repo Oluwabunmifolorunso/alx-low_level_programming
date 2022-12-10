38292fc88e5ac4229b40c69a6e1ba48b1f37afcd
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-09 19:35:07 -0800

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the letters except q and e in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	char sl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(sl);
+	}
+	putchar('\n');
+	return (0);
+}
