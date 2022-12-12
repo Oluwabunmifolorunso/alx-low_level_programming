c586feeb2352c3d40c1d3a767c7c30ed59f0caf4
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-11 16:02:22 -0800

    The success combination in business

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - program that prints possible different combinations of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int o;
+	int t;
+		for (o = 0; o <= 99; o++)
+	{
+		for (t = 0; t <= 99; t++)
+		{
+			if (t > o)
+			{
+				putchar(o / 10 + '0');
+				putchar(o % 10 + '0');
+				putchar(' ');
+				putchar(t / 10 + '0');
+				putchar(t % 10 + '0');
+				if (o * 100 + t != 9899)
+				{
+					putchar(',');
+					putchar(' ')
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
