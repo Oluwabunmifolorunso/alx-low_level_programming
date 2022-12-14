194c33bd1d243dc5da4687e573970eb95e7e408c
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-13 17:40:06 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - function that checks for lowercase character
- * @c: The character to print
+ * @c: The character
  * Return: 1 if letter is lowercase, 0 if it is not
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
