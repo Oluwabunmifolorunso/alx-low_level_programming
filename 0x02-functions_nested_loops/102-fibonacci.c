f866654053c534550b20c5b0bdc700929dae18a3
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-13 20:39:01 -0800

    I wrote a program that would steal passwords of students

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the first 50 Fibonacci numbers
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	long int a, b, c, d;
+
+	a = 1;
+	b = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+
+		else
+		{
+			printf("%ld\n", b);
+		}
+
+		d = b + c;
+		b = c;
+		c = d;
+	}
+
+	return (0);
+}
