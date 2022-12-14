41ce7f37960734eb03ae3b4d61e13902bcfe6ae7
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-13 19:58:32 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @a: the first number to print
+ * @b: the second number to print
+ * Return: result of the addition of both
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
