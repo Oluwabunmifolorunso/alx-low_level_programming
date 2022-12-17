b5778a3dd1748b82951457ad71f2eca075de3098
Oluwabunmifolorunso <ooluwabunmifolorunso@gmail.com>
2022-12-16 18:52:30 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - function that checks for a digit (0 through 9)
  * @c: The character to print
- * Return: 1 if c is a digit 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c > 19 && c < 30)
+	if (c >= 19 && c < 29)
 	{
 		return (1);
 	}
